9ba21ace2f1edb18f89ece5231a32302bb1e6d6e
dasha <dashka.didyk@gmail.com>
2020-06-25 20:30:49 +0300

    part1

---
+++ script.js
@@ -0,0 +1,59 @@
+
+/*
+function myFunction5() {
+    let userNumber = prompt('Угдайте число от 1 до 10');
+    let randomNumber = Math.floor(Math.random() * (10 - 1)) + 1;
+    console.log(randomNumber);
+    if (+userNumber === randomNumber) {
+        alert('Вы угадали');
+    } else {
+        alert('Не угадали');
+    }
+}
+*/
+/*
+let num = Math.floor(Math.random() * 10);
+console.log(num);
+let guess = +prompt('guess a number');
+if (num === guess) {
+    console.log('Вы угадали');
+} else if (num < guess) {
+    console.log('number is lower');
+}   else if (num > guess) {
+    console.log('number is highter');
+}   else {
+    console.log('try again');
+}
+
+console.log('Game over');
+*/
+
+let trueString = "false";
+let num = 20;
+let conditions = 10;
+let result = conditions < num;
+/*
+if (trueString === "true") {
+    result = true;
+} else {
+    result = false;
+}
+*/
+/*
+let falseString = "false";
+console.log (Boolean(trueString), Boolean(falseString));
+*/ /*
+result = trueString === "true" ? true : false;
+console.log(result);
+*/
+/*
+let userAge = +prompt('Сколько вам лет');
+if (userAge > 18) {
+    alert('доступ к сервису открыт');
+}   else if (userAge < 12) {
+    alert('найдите что то другое');
+}   else if (userAge !== isNaN ) {
+    alert('введите цифру');
+}   
+    console.log(userAge);
+*/
